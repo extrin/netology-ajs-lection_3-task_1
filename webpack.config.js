0bf113c9421d093335de2f2018ae18b72ea2bb21
E.S <extrin13@gmail.com>
2019-03-31 01:27:25 +0300

    feat: Use webpack devserver

--- webpack.config.js
+++ webpack.config.js
@@ -8,21 +8,26 @@ module.exports = {
     rules: [
       {
         test: /\.html$/,
-        use: [ { loader: "html-loader" }, ],
+        use: [{ loader: 'html-loader' }],
       },
       {
         test: /\.css$/,
         use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: 'dist'
+            }
+          },
+          "css-loader"
         ],
       },
     ],
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
@@ -34,4 +39,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js',
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
 };
